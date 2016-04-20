4a525f39b8406ca5bae5f5687c687d8d4bb30994
dsouzadyn <dsouzadyn@gmail.com>
2016-04-20 12:58:38 +0530

    Program 9 done.

---
+++ program9/program9.c
@@ -0,0 +1,71 @@
+/*
+*
+*	Program to find maximum and minimum in array.	
+*
+*/
+
+#include <stdio.h>
+
+// Uncomment the line below if executing on Windows.
+//#include <conio.h>
+
+int main()
+{
+	/*
+	*	Declare variable to store the counts.
+	*/
+	int i, j;
+
+	/*
+	*	Declare and initialize an array.
+	*/
+	int ar[] = {3, 0, -1, 2, 5, 99};
+
+	/*
+	*	Declare variables to store max and min value.
+	*/
+	int max, min;
+
+	/*
+	*	Initialise both to the first element of the array;
+	*/
+	max = ar[0];
+	min = ar[0];
+
+	/*
+	*	Find the maximum value in the array.
+	*/
+	for(i = 1; i <= 6; i++)
+	{
+		if (ar[i] > max)
+		{
+			max = ar[i];
+		}
+	}
+
+	/*
+	*	Find the minimum value in the array.
+	*/
+	for(j = 1; j <= 6; j++)
+	{
+		if(ar[j] < min)
+		{
+			min = ar[j];
+		}
+	}
+
+	/*
+	*	Print the maximum value.
+	*/
+	printf("Maximum: %d\n", max);
+
+	/*
+	*	Print the minimum value.
+	*/
+	printf("Minimum: %d\n", min);
+
+	// Uncomment the line below if compiling on Windows.
+	//getch();
+
+	return 0;
+}
